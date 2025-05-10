8ed38c7d502845bec2e6f23a4c14622cf23271a1
eldoz <81470244+eldoz@users.noreply.github.com>
2025-05-10 14:43:35 +0200

    eldoniii

---
+++ main.js
@@ -0,0 +1,93 @@
+console.log(numriIDyte)
+
+x--;
+x++;
+
+console.log(x)
+
+
+console.log("------------------")
+var x = 15;
+x+=5
+//x= x+5
+console.log(x)
+
+
+
+console.log("------------------")
+var y = 20;
+y+=10
+//y= y+10
+console.log(y)
+
+
+
+
+console.log("------------------")
+var k = 20;
+k*=2
+//k= k*10
+console.log(k)
+
+
+console.log("------------------")
+var l = 20;
+l= 1/10
+console.log(l)
+
+
+
+console.log("-----pjesa e dyte-----")
+
+
+var x = 5;
+var y = 5;
+
+console.log(x==y)
+
+var e = 3;
+var f = 5;
+
+console.log(e==f)
+
+
+var t = 15;
+var e = 16;
+console.log("-------------")
+
+console.log(p!=u)
+
+
+var num1=100
+var num2=150
+
+console.log("----------------")
+console.log(num1>num2)
+
+console.log("----------------")
+console.log(num1<num2)
+
+
+var age=25
+
+
+console.log(age>18 && age<65)
+
+var var1= 5;
+var var2=6;
+
+console.log(var1==5 || var2==5)
+
+
+var firstName= "Eldo"
+var lastName= "Pervo"
+
+
+console.log(firstName+""+lastName)
+
+
+
+
+
+
+
